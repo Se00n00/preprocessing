5025cea86b2d11889199386a704d4fba802f6f11
Mohit <bluesky5april2021@gmail.com>
2025-02-16 04:06:31 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
+import { LowerCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TitleCasePipe,LowerCasePipe,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'preprocessing';
+  currentRoute = signal("Text")
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+import { LicenseComponent } from './license/license.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",title:"Preprocessing", component:HomeComponent},
+    {path:"about",title:"about me",component:AboutComponent},
+    {path:"license",title:"license",component:LicenseComponent},
+    {path:"**",title:"oops! page not found",component:NotfoundComponent}
+];
